e89cc160383ff79bdc7650becde711ebe8a488e8
nguyentruong0607 <nguyenxuantruong2k3tb@gmail.com>
2024-06-13 23:20:30 +0700

    #1-nguyentruong0607 codeViewsLogin

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+console.log("11111");
+mongoose.connect('mongodb://127.0.0.1:27017/duAnThucTap')
+
+    .catch((err) => {
+        console.log("Loi ket noi CSDL");
+        console.log(err);
+    })
+    .finally((xxx)=>{
+        console.log(xxx);
+        console.log("ket noi csdl ok");
+    })
+module.exports={mongoose};
+console.log("zzzzzk");
