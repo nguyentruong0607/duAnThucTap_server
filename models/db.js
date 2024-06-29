ffc95fb9d1884b5eb6b20df98c5f61dd87191458
DT Quang <quangdtph21898@fpt.edu.vn>
2024-06-30 02:19:00 +0700

    quangdtph21898 suaMongodb

--- models/db.js
+++ models/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 console.log("11111");
-mongoose.connect('mongodb://127.0.0.1:27017/duAnThucTap')
+// mongoose.connect('mongodb://127.0.0.1:27017/duAnThucTap')
+mongoose.connect('mongodb+srv://quangdtph21898:6af2z7GWK8X0NGEW@cluster0.qiehtpv.mongodb.net/DATN')
+
 
     .catch((err) => {
         console.log("Loi ket noi CSDL");
